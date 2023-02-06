5fdb16e9d118ffbb7ac21fdf51bb416a56787246
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2023-02-06 00:49:10 -0500

    update sprite

--- Tiles/Furniture/Trophies/NovaTrophyTile.cs
+++ Tiles/Furniture/Trophies/NovaTrophyTile.cs
@@ -9,25 +9,26 @@ using Terraria.ObjectData;
 
 namespace ShardsOfAtheria.Tiles.Furniture.Trophies
 {
-	// Simple 3x3 tile that can be placed on a wall
-	public class NovaTrophyTile : ModTile
-	{
-		public override void SetStaticDefaults()
-		{
-			Main.tileFrameImportant[Type] = true;
-			Main.tileLavaDeath[Type] = true;
-			TileID.Sets.FramesOnKillWall[Type] = true;
+    // Simple 3x3 tile that can be placed on a wall
+    public class NovaTrophyTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleWrapLimit = 36;
+            TileObjectData.addTile(Type);
+            DustType = DustID.WoodFurniture;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            AddMapEntry(new Color(120, 85, 60), Language.GetText("MapObject.Trophy"));
+        }
 
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
-			TileObjectData.addTile(Type);
-
-			AddMapEntry(new Color(120, 85, 60), Language.GetText("MapObject.Trophy"));
-			DustType = 7;
-		}
-
-		public override void KillMultiTile(int i, int j, int frameX, int frameY)
-		{
-			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<NovaTrophy>());
-		}
-	}
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<NovaTrophy>());
+        }
+    }
 }
