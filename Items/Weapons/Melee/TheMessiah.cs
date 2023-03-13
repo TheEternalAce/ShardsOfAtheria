f8e2b17a50a6805a744e71abbf197672e460386c
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2023-03-12 21:27:51 -0400

    fix broken custom sounds

--- Items/Weapons/Melee/TheMessiah.cs
+++ Items/Weapons/Melee/TheMessiah.cs
@@ -69,7 +69,7 @@ namespace ShardsOfAtheria.Items.Weapons.Melee
         {
             if (!theMessiah && Main.myPlayer == player.whoAmI)
             {
-                SoundEngine.PlaySound(new SoundStyle($"{nameof(ShardsOfAtheriaMod)}/Sounds/Item/TheMessiah"));
+                SoundEngine.PlaySound(new SoundStyle("ShardsOfAtheria/Sounds/Item/TheMessiah"));
                 theMessiah = true;
             }
             if (charge < 200)
