a70c87e7351bcaa7757bd04ad630b4b8031ff927
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2024-01-30 13:24:29 -0500

    change Cheater's Glove to Weighted Die

--- Items/Tools/Misc/WeightedDie.cs
+++ Items/Tools/Misc/WeightedDie.cs
@@ -6,16 +6,14 @@ using Terraria.ModLoader;
 
 namespace ShardsOfAtheria.Items.Tools.Misc
 {
-    public class CheatersGlove : ModItem
+    public class WeightedDie : ModItem
     {
         public int cheatSide = 6;
 
-        public override string Texture => SoA.PlaceholderTexture;
-
         public override void SetDefaults()
         {
-            Item.width = 32;
-            Item.height = 32;
+            Item.width = 44;
+            Item.height = 44;
 
             Item.rare = ItemDefaults.RarityAreus;
             Item.value = ItemDefaults.ValueDungeon;
@@ -38,12 +36,12 @@ namespace ShardsOfAtheria.Items.Tools.Misc
                 cheatSide = 6;
             }
             var shards = player.Shards();
-            shards.cheatGlove = cheatSide;
+            shards.weightDie = cheatSide;
         }
 
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
-            var key = this.GetLocalizationKey(string.Empty) + "Off";
+            var key = this.GetLocalizationKey("Off");
             if (cheatSide > 0)
             {
                 tooltips[0].Text += " (" + (cheatSide) + ")";
