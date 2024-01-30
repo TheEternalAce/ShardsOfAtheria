cc88ff68c72d0086211a93cac956a16fc417a352
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2024-01-30 13:12:29 -0500

    Add Areus Blade Staff

---
+++ Projectiles/Summon/Minions/AreusStabber.cs
@@ -0,0 +1,247 @@
+﻿using Microsoft.Xna.Framework;
+using ShardsOfAtheria.Buffs.Summons;
+using ShardsOfAtheria.Utilities;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Projectiles.Summon.Minions
+{
+    public class AreusStabber : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            // This is necessary for right-click targeting
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+
+            Main.projPet[Projectile.type] = true; // Denotes that this projectile is a pet or minion
+
+            ProjectileID.Sets.MinionSacrificable[Type] = true;
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+
+            Projectile.AddAreus();
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.netImportant = true;
+            Projectile.width = 6;
+            Projectile.height = 6;
+            Projectile.aiStyle = -1;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 600;
+            Projectile.minion = true;
+            Projectile.minionSlots = 1f;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 10;
+
+            DrawOffsetX = -4;
+        }
+
+        // Here you can decide if your minion breaks things like grass or pots
+        public override bool? CanCutTiles()
+        {
+            return false;
+        }
+
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+
+        public override void AI()
+        {
+            Player owner = Main.player[Projectile.owner];
+
+            Projectile.netUpdate = true;
+            if (!CheckActive(owner))
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            Visuals();
+            GeneralBehavior(owner, out Vector2 vectorToIdlePosition, out float distanceToIdlePosition);
+            SearchForTargets(owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter);
+            Movement(foundTarget, distanceFromTarget, targetCenter, distanceToIdlePosition, vectorToIdlePosition);
+        }
+
+        private void GeneralBehavior(Player owner, out Vector2 vectorToIdlePosition, out float distanceToIdlePosition)
+        {
+            Vector2 idlePosition = owner.Center;
+            idlePosition.Y -= 45f;
+
+            float minionPositionOffsetX = (10 + Projectile.minionPos * 40) * -owner.direction;
+            idlePosition.X += minionPositionOffsetX;
+
+            // All of this code below this line is adapted from Spazmamini code (ID 388, aiStyle 66)
+
+            // Teleport to player if distance is too big
+            vectorToIdlePosition = idlePosition - Projectile.Center;
+            distanceToIdlePosition = vectorToIdlePosition.Length();
+
+            if (distanceToIdlePosition > 2500f)
+            {
+                Projectile.Center = owner.Center;
+                Projectile.netUpdate = true;
+            }
+
+            // If your minion is flying, you want to do this independently of any conditions
+            float overlapVelocity = 0.04f;
+
+            // Fix overlap with other minions
+            for (int i = 0; i < Main.maxProjectiles; i++)
+            {
+                Projectile other = Main.projectile[i];
+
+                if (i != Projectile.whoAmI && other.active && other.owner == Projectile.owner &&
+                    Math.Abs(Projectile.position.X - other.position.X) + Math.Abs(Projectile.position.Y - other.position.Y) < Projectile.width)
+                {
+                    if (Projectile.position.X < other.position.X)
+                    {
+                        Projectile.velocity.X -= overlapVelocity;
+                    }
+                    else
+                    {
+                        Projectile.velocity.X += overlapVelocity;
+                    }
+
+                    if (Projectile.position.Y < other.position.Y)
+                    {
+                        Projectile.velocity.Y -= overlapVelocity;
+                    }
+                    else
+                    {
+                        Projectile.velocity.Y += overlapVelocity;
+                    }
+                }
+            }
+        }
+
+        private void SearchForTargets(Player owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter)
+        {
+            // Starting search distance
+            distanceFromTarget = 700f;
+            targetCenter = Projectile.position;
+            foundTarget = false;
+
+            // This code is required if your minion weapon has the targeting feature
+            if (owner.HasMinionAttackTargetNPC)
+            {
+                NPC npc = Main.npc[owner.MinionAttackTargetNPC];
+                float between = Vector2.Distance(npc.Center, Projectile.Center);
+
+                // Reasonable distance away so it doesn't target across multiple screens
+                if (between < 2500f)
+                {
+                    distanceFromTarget = between;
+                    targetCenter = npc.Center;
+                    foundTarget = true;
+                }
+            }
+
+            if (!foundTarget)
+            {
+                // This code is required either way, used for finding a target
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+
+                    if (npc.CanBeChasedBy())
+                    {
+                        float between = Vector2.Distance(npc.Center, Projectile.Center);
+                        bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        bool lineOfSight = Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+                        // Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
+                        // The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
+                        bool closeThroughWall = between < 100f;
+
+                        if ((closest && inRange || !foundTarget) && (lineOfSight || closeThroughWall))
+                        {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Movement(bool foundTarget, float distanceFromTarget, Vector2 targetCenter, float distanceToIdlePosition, Vector2 vectorToIdlePosition)
+        {
+            float speed = 4f;
+            float inertia = 60f;
+
+            if (foundTarget)
+            {
+                if (distanceFromTarget > 80f)
+                {
+                    speed = 60f;
+                    inertia = 20;
+                    Vector2 direction = targetCenter - Projectile.Center;
+                    direction.Normalize();
+                    direction *= speed;
+
+                    Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
+                }
+            }
+            else
+            {
+                if (++Projectile.ai[1] >= 60)
+                {
+                    Projectile.ai[1] = 0;
+                }
+
+                // Minion doesn't have a target: return to player and idle
+                if (distanceToIdlePosition > 300f)
+                {
+                    // Speed up the minion if it's away from the player
+                    speed = 12f;
+                    inertia = 40f;
+                }
+
+                if (distanceToIdlePosition > 20f)
+                {
+                    // The immediate range around the player (when it passively floats about)
+
+                    // This is a simple movement formula using the two parameters and its desired direction to create a "homing" movement
+                    vectorToIdlePosition.Normalize();
+                    vectorToIdlePosition *= speed;
+                    Projectile.velocity = (Projectile.velocity * (inertia - 1) + vectorToIdlePosition) / inertia;
+                }
+                else if (Projectile.velocity == Vector2.Zero)
+                {
+                    // If there is a case where it's not moving at all, give it a little "poke"
+                    Projectile.velocity.X = -0.15f;
+                    Projectile.velocity.Y = -0.05f;
+                }
+            }
+        }
+
+        private bool CheckActive(Player owner)
+        {
+            if (owner.dead || !owner.active || !owner.HasBuff<AreusStabberBuff>())
+                return false;
+            Projectile.timeLeft = 2;
+            return true;
+        }
+
+        private void Visuals()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.spriteDirection = Projectile.direction;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            lightColor = Color.White;
+            return base.PreDraw(ref lightColor);
+        }
+    }
+}
