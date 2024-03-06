6e142f481c9851eb47057e59afdaffb5aecf25d8
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2024-03-06 04:51:40 -0500

    Add BURST

---
+++ Items/Weapons/Ranged/HansMachineGun.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using ShardsOfAtheria.Projectiles.Ranged;
+using ShardsOfAtheria.Utilities;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Items.Weapons.Ranged
+{
+    public class HansMachineGun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.AddElement(2);
+            Item.AddRedemptionElement(7);
+            Item.AddRedemptionElement(15);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 198;
+            Item.height = 76;
+            Item.DefaultToRangedWeapon(ProjectileID.PurificationPowder, AmmoID.Bullet, 3, 16f);
+
+            Item.damage = 50;
+            Item.knockBack = 4f;
+
+            Item.useAnimation = 600;
+            Item.UseSound = SoundID.Item38;
+            Item.consumeAmmoOnFirstShotOnly = true;
+
+            Item.rare = ItemRarityID.Cyan;
+            Item.value = Item.sellPrice(0, 4, 25);
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-45, -20);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            velocity = velocity.RotatedByRandom(MathHelper.ToRadians(5));
+            if (type == ProjectileID.Bullet)
+            {
+                type = ModContent.ProjectileType<HansBullet>();
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            SoundEngine.PlaySound(Item.UseSound, player.Center);
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+        }
+    }
+}
---
+++ Projectiles/Ranged/HansBullet.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using ShardsOfAtheria.Projectiles.Other;
+using ShardsOfAtheria.Utilities;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Projectiles.Ranged
+{
+    public class HansBullet : ModProjectile
+    {
+        public override string Texture => SoA.BlankTexture;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 20;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+            Projectile.AddElement(2);
+            Projectile.AddRedemptionElement(7);
+            Projectile.AddRedemptionElement(15);
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 6; // The width of projectile hitbox
+            Projectile.height = 6; // The height of projectile hitbox
+            Projectile.aiStyle = 1; // The ai style of the projectile, please reference the source code of Terraria
+            Projectile.friendly = true; // Can the projectile deal damage to enemies?
+            Projectile.DamageType = DamageClass.Ranged; // Is the projectile shoot by a ranged weapon?
+            Projectile.timeLeft = 600; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+            Projectile.light = 0.5f; // How much light emit around the projectile
+            Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+            Projectile.extraUpdates = 1; // Set to above 0 if you want the projectile to update multiple time in a frame
+
+            AIType = ProjectileID.WoodenArrowFriendly; // Act exactly like default Bullet
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            return base.OnTileCollide(oldVelocity);
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            var explosion = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero,
+                ModContent.ProjectileType<ElectricExplosion>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+            explosion.DamageType = Projectile.DamageType;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            lightColor = SoA.ElectricColor;
+            Projectile.DrawBlurTrail(lightColor, SoA.LineBlur);
+            return base.PreDraw(ref lightColor);
+        }
+    }
+}
