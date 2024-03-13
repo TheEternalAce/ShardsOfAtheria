8aea64151351352c4b70166b0b9f2cb26b4ed750
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2024-03-13 17:13:27 -0400

    add Gamma Plague Handgun

---
+++ Items/Weapons/Ranged/PlagueHandgun.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using ShardsOfAtheria.Items.Placeable;
+using ShardsOfAtheria.Items.Weapons.Ammo;
+using ShardsOfAtheria.Projectiles.Ranged.PlagueRail;
+using ShardsOfAtheria.Utilities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Items.Weapons.Ranged
+{
+    public class PlagueHandgun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.AddElement(3);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 50;
+            Item.height = 28;
+            Item.DefaultToRangedWeapon(ProjectileID.PurificationPowder, AmmoID.Bullet, 24, 16f);
+
+            Item.damage = 15;
+            Item.knockBack = 2f;
+
+            Item.UseSound = SoundID.Item41;
+            Item.autoReuse = true;
+
+            Item.rare = ItemDefaults.RarityMechs;
+            Item.value = Item.sellPrice(0, 4, 25);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<BionicBarItem>(15)
+                .AddIngredient(ItemID.HallowedBar, 5)
+                .AddIngredient<PlagueCell>(20)
+                .AddIngredient(ItemID.Wire, 20)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+
+        public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            Item plagueRail = ModContent.GetInstance<PlagueRailgun>().Item;
+            return !player.HasAmmo(plagueRail) && !player.Overdrive();
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (type == ProjectileID.Bullet)
+            {
+                Item plagueRail = ModContent.GetInstance<PlagueRailgun>().Item;
+                if (player.HasAmmo(plagueRail))
+                {
+                    player.PickAmmo(plagueRail, out int _, out float _, out int _, out float _, out int _);
+                    type = ModContent.ProjectileType<PlagueBullet>();
+                }
+            }
+        }
+
+        public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
+        {
+            int multiplier = 1;
+            if (NPC.downedMoonlord)
+            {
+                multiplier++;
+            }
+            if (NPC.downedAncientCultist)
+            {
+                multiplier++;
+            }
+            if (NPC.downedGolemBoss)
+            {
+                multiplier++;
+            }
+            if (NPC.downedPlantBoss)
+            {
+                multiplier++;
+            }
+            if (NPC.downedMechBossAny)
+            {
+                multiplier++;
+            }
+            if (Main.hardMode)
+            {
+                multiplier++;
+            }
+            if (NPC.downedBoss3)
+            {
+                multiplier++;
+            }
+            if (NPC.downedBoss2)
+            {
+                multiplier++;
+            }
+            if (NPC.downedBoss1)
+            {
+                multiplier++;
+            }
+            damage *= multiplier;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(0);
+        }
+    }
+}
---
+++ Projectiles/Ranged/PlagueRail/PlagueBullet.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using ShardsOfAtheria.Utilities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Projectiles.Ranged.PlagueRail
+{
+    public class PlagueBullet : ModProjectile
+    {
+        public override string Texture => SoA.BlankTexture;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.PlayerHurtDamageIgnoresDifficultyScaling[Type] = true;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+            ProjectileID.Sets.TrailCacheLength[Type] = 20;
+            Projectile.AddElement(3);
+            Projectile.AddRedemptionElement(11);
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 4;
+            Projectile.timeLeft = 600;
+            Projectile.extraUpdates = 5;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.aiStyle = 0;
+            Projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            int dustDelay = 11;
+            if (++Projectile.ai[0] >= dustDelay)
+            {
+                if (!Projectile.hostile)
+                {
+                    Projectile.hostile = true;
+                }
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            lightColor = Color.PaleGreen;
+            Projectile.DrawBlurTrail(lightColor, SoA.LineBlur);
+            return base.PreDraw(ref lightColor);
+        }
+    }
+}
