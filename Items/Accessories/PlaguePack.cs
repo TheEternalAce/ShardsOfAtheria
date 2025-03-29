ab1db6dd341e7374da225dc57da2da9336137913
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2025-03-29 02:36:18 -0400

    add Plague Pack

---
+++ Items/Accessories/PlaguePack.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShardsOfAtheria.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Back)]
+    public class PlaguePack : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 28;
+            Item.accessory = true;
+
+            Item.rare = ItemRarityID.Quest;
+            Item.value = 100000;
+        }
+    }
+}
